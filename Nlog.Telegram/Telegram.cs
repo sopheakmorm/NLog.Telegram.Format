dbe3e8707b60dfb9f17c1e1daba15df624fee359
morm.sophak <morm.sophak@ABAHUBLTDIGPAK>
2020-11-07 12:14:29 +0700

    commit source code

---
+++ Nlog.Telegram/Telegram.cs
@@ -0,0 +1,26 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlog.Telegram
+{
+    [Target("Telegram")]
+    public sealed class TelegramTarget : TargetWithLayout
+    {
+        public TelegramTarget()
+        {
+        }
+
+        [RequiredParameter]
+        public string Host { get; set; }
+        protected override void Write(LogEventInfo logEvent)
+        {
+            string logMessage = this.Layout.Render(logEvent);
+        }
+    }
+}
